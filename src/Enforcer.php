ea152720b8fe763396fb42ad0af673358b101723
weiwei163@foxmail.com <ylqchengwei5566~>
2021-02-22 19:53:53 +0800

    add: 允许插入自己服务

--- src/Enforcer.php
+++ src/Enforcer.php
@@ -2,6 +2,7 @@
 
 namespace teamones\registerRoute;
 
+use think\facade\Db;
 use teamones\http\Client;
 
 class Enforcer
@@ -14,14 +15,88 @@ class Enforcer
 
     protected $_url = "";
 
+    protected $_isSelf = false;
+
     /**
      * Enforcer constructor.
      * @param string $url
+     * @param bool $isSelf
      */
-    public function __construct(string $url)
+    public function __construct(string $url, $isSelf = false)
     {
         $this->_instance = new Client();
         $this->_url = $url;
+        $this->_isSelf = $isSelf;
+    }
+
+    /**
+     * 通过数据库直接更新记录
+     */
+    protected function saveRecordByDB($data)
+    {
+        try {
+            $dbConfig = config('thinkorm');
+            $dbConfig['break_reconnect'] = false;
+            Db::setConfig($dbConfig);
+
+
+            // 1. 组装查询条件
+            // - belong_system
+            // - method
+            // - record
+            // 上面三者联合唯一
+            $filterBelongSystem = [];
+            $filterMethod = [];
+            $filterRecord = [];
+            foreach ($data as $item) {
+                if (!empty($item['belong_system']) && !in_array($item['belong_system'], $filterBelongSystem)) {
+                    $filterBelongSystem[] = $item['belong_system'];
+                }
+
+                if (!empty($item['method']) && !in_array($item['method'], $filterMethod)) {
+                    $filterMethod[] = $item['method'];
+                }
+
+                if (!empty($item['record']) && !in_array($item['record'], $filterRecord)) {
+                    $filterRecord[] = $item['record'];
+                }
+            }
+
+            $ExistRoutes = Db::table('route')
+                ->whereIn("belong_system", $filterBelongSystem)
+                ->whereIn("method", $filterMethod)
+                ->whereIn("record", $filterRecord)
+                ->select();
+
+            // 2. 查询重复记录
+            $ExistRoutesMap = [];
+            foreach ($ExistRoutes as $ExistRoute) {
+                $keyMd5 = md5("{$ExistRoute['belong_system']}_{$ExistRoute['record']}_{$ExistRoute['method']}");
+                if (!in_array($keyMd5, $ExistRoutesMap)) {
+                    $ExistRoutesMap[] = $keyMd5;
+                }
+            }
+
+            // 3. 写入非重复记录
+            $saveData = [];
+            foreach ($data as $item) {
+                $keyMd5 = md5("{$item['belong_system']}_{$item['record']}_{$item['method']}");
+                if (!in_array($keyMd5, $ExistRoutesMap)) {
+                    $saveData[] = $item;
+                }
+            }
+
+            if (empty($saveData)) {
+                return [];
+            }
+
+            Db::table('route')->insertAll($saveData);
+
+            // 处理完释放连接
+            Db::connect()->close();
+        } catch (\Exception $e) {
+            var_dump($e->getMessage());
+        }
     }
 
     /**
@@ -30,12 +105,17 @@ class Enforcer
      */
     protected function registerRemote($postData = [])
     {
-        try {
-            $response = $this->_instance->setHost($this->_url)
-                ->setBody(['data' => $postData])
-                ->setMethod("POST")
-                ->request();
-        } catch (\Exception $e) {
+        if ($this->_isSelf) {
+            $this->saveRecordByDB($postData);
+        } else {
+            try {
+                $response = $this->_instance->setHost($this->_url)
+                    ->setBody(['data' => $postData])
+                    ->setMethod("POST")
+                    ->request();
+            } catch (\Exception $e) {
+
+            }
         }
     }
 
